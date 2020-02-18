2d7ab099c670107d6fda57ca951b20968d57d85a
Max McCormack <47184910+Bluespongecake@users.noreply.github.com>
2020-02-18 17:19:49 +0100

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -22,8 +22,10 @@
 //Hexadecimal I2C bus address. Should be 39. Check back of IIC backpack module for 4 solderpoints that set address
 const int I2C = 0x27; 
 
-#define Clock 3
-#define Data 4
+#define TX_CLOCK 3
+#define TX_DATA 4
+
+#define TX_RATE 40  //Transmit rate in bits per second
 
 //---------------------------------------------------------------------------
 
@@ -33,7 +35,7 @@ const char *message = "BT Young Scientist"; // message to be sent
 
 //---------------------------------------------------------------------------
 
-//defining the pins for the LCD (from the I2C (a type of bus))
+//defining the pins for the LCD (from the I2C (bus))
 LiquidCrystal_I2C lcd(I2C,20,4); //20 char 4 rows
 
 void setup(){
@@ -45,7 +47,7 @@ void setup(){
   lcd.init();
   lcd.backlight();
   
-  Serial.begin(9600);                                              //always useful
+  //Serial.begin(9600);                                //always useful
 
   lcd.print("starting tx...");
   delay(500);
@@ -59,28 +61,28 @@ void setup(){
   lcd.print("0/");
 
   //main operation. bytec = byte counter
-  for (int byte_idx = 0; bytec < strlen(message); bytec++) {
-    char tbyte = message[bytec];
+  for (int byteC = 0; byteC < strlen(message); byteC++) {
+    char tbyte = message[byteC];
 
     // Clear the 3rd line of the display
     lcd.noCursor();
     lcd.setCursor(0, 2);
     lcd.print("        ");
-    lcd.setCursor(byte_idx, 0);
+    lcd.setCursor(byteC, 0);
     lcd.cursor();
 
     for (int bitc = 0; bitc < 8; bitc++) {                         //bitc is bit counter
       bool tbit = tbyte & (0x80 >> bitc);                          //core line. Took ages to figure out. Goddamit.
 
-      digitalWrite(DATA, tbit);
+      digitalWrite(TX_DATA, tbit);
       delay((1000 / TX_RATE) / 2);
 
       // Update the LCD and serial monitor
       lcd.noCursor();
       lcd.setCursor(bitc, 1);
       lcd.print(tbit ? "1" : "0");
-      serial.println(tbit ? "1" : "0");
-      lcd.setCursor(byte_idx, 0);
+//      serial.println(tbit ? "1" : "0");                          //debugging
+      lcd.setCursor(byteC, 0);
       lcd.cursor();
 
       // Pulse clock
@@ -92,7 +94,6 @@ void setup(){
   digitalWrite(TX_DATA, LOW);
 }
     
-}
 
 void loop(){
   //dunno why I left this here. Force of Habit I guess.
